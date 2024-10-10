cb00c928f31c8de4355ce46103101dda3e463a98
tristan-gscn <tristan.gscn@gmail.com>
2024-10-10 16:39:25 +0200

    Update libs in start.sh

--- start.sh
+++ start.sh
@@ -1,38 +1,29 @@
 #!/bin/bash
 
-# Detect operating system
-if [[ "$(uname)" == "Linux" ]]; then
-    if command -v apt >/dev/null 2>&1; then
-        # Debian Based
-        echo 
+if [ "$(uname)" == "Linux" ]; then
+    if command -v apt-get >/dev/null 2>&1; then
+        # Debian/Ubuntu Based
+        yes | sudo apt-get update
+        yes | sudo apt-get install python3 python3-pip python3-venv
+        yes | sudo apt-get install libmysqlclient-dev  # Ajout de cette ligne
+        python3 -m venv venv
+        source venv/bin/activate
+        yes | pip install flask
+        yes | pip install flask-mysqldb
+        sudo python3 app.py
+    elif command -v pacman >/dev/null 2>&1; then
+        echo
         echo Welcome to $(grep -oP '^NAME="\K[^"]+' /etc/os-release).
         echo To run this script, you need to be root.
         echo
-        yes | sudo apt update
-        yes | sudo apt install python3 
-        yes | sudo apt install python3-pip
-        yes | sudo apt install python$(python3 -V 2>&1 | awk -F'[ .]' '{print $2"."$3}')-venv
-        python3 -m venv venv
-        source venv/bin/activate
-        yes | sudo apt install python3
-        yes | pip install flask
-        yes | pip install Flask-MySQL
-        sudo python3 app.py
-    elif command -v pacman >/dev/null 2>&1; then
-    echo 
-    echo Welcome to $(grep -oP '^NAME="\K[^"]+' /etc/os-release).
-    echo To run this script, you need to be root.
-    echo
         # Arch Based
         yes | sudo pacman -Syu
-        yes | sudo pacman -S python3
-        yes | sudo pacman -S python-pip
-        yes | sudo pacman -S python-virtualenv
+        yes | sudo pacman -S python3 python-pip python-virtualenv
+        yes | sudo pacman -S mariadb-libs  # Ajout de cette ligne
         python3 -m venv venv
         source venv/bin/activate
-        yes | sudo pacman -S python3
         yes | pip install flask
-        yes | pip install Flask-MySQL
+        yes | pip install flask-mysqldb
         sudo python3 app.py
     else
         echo "OS not supported"
